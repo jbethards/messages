78f1d464e43748ddf1ab867ee4c995dc14477f9e
jbethards <jbethards2984@smail.pcd.edu>
2018-04-12 20:34:39 -0700

    Added the insert Messages php file

--- pages/insertMessages.php
+++ pages/insertMessages.php
@@ -4,4 +4,23 @@
  * User: Joseph Bethards
  * Date: 4/12/2018
  * Time: 8:29 PM
- */
+ */?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css">
+</head>
+
+<body>
+<!-- Place your content here -->
+
+</body>
+</html>
